c3475e674b637962a074e9076c1ac41965c09671
AJP <allenatmc@gmail.com>
2016-03-06 20:34:37 -0600

    finish scoring logic and handle alternating turns

--- client/scripts/controllers/yahtzeeController.js
+++ client/scripts/controllers/yahtzeeController.js
@@ -1,11 +1,15 @@
 .controller('YahtzeeController', ['$log', 'diceService', 'playerService', function($log, $dice, $player) {
   var vm = this
-  vm.title = "Yahtzee"
+
+  vm.numRolls = 3;
+  var turn = 0;
 
   vm.players = [
     $player.newPlayer('Player1'),
     $player.newPlayer('Player2')
   ]
+
+  vm.currentPlayer = vm.players[0]
 
   vm.dice = $dice.getDice();
 
@@ -26,14 +30,29 @@
   }
 
   vm.rollAll = function() {
+    if (vm.numRolls > 0){
 
-    vm.dice.forEach(function(die, index) {
-      if(!die.keeper) {
-       vm.roll(die)
-      }
+      vm.dice.forEach(function(die, index) {
+        if(!die.keeper) {
+         vm.roll(die)
+        }
 
-    })
+      })
 
+    }
+
+    vm.numRolls--
+  }
+
+  //initialize die roll
+  vm.rollAll()
+
+  //on a turn end, increment current turn, re-roll die, and update current player
+  vm.endTurn = function(){
+    turn += 1
+    vm.numRolls = 3
+    vm.rollAll()
+    vm.currentPlayer = vm.players[turn % vm.players.length]
   }
 
 
--- client/scripts/services/player.js
+++ client/scripts/services/player.js
@@ -24,9 +24,47 @@
 
     this.scoreField = function(diceArray, fieldName) {
 
-      $log.log('score called')
 
       var points = 0;
+
+      var num1s = 0;
+      var num2s = 0;
+      var num3s = 0;
+      var num4s = 0;
+      var num5s = 0;
+      var num6s = 0;
+
+      diceArray.forEach(function(die){
+
+
+        if (die.value == 1) {
+          num1s += 1;
+        }
+
+        if (die.value == 2) {
+          num2s += 1;
+        }
+
+        if (die.value == 3) {
+          num3s += 1;
+        }
+
+        if (die.value == 4) {
+          num4s += 1;
+        }
+
+        if (die.value == 5) {
+          num5s += 1;
+        }
+
+        if (die.value == 6) {
+          num6s += 1;
+        }
+
+      })
+
+      var totals = [num1s, num2s, num3s, num4s, num5s, num6s];
+
       switch (fieldName) {
 
 
@@ -139,7 +177,96 @@
 
           break;
 
+        case 'threeOfAKind':
 
+          this.score.threeOfAKind = 'Bust'
+
+          totals.forEach(function(number){
+            if (number >= 3 ) {
+              diceArray.forEach(function(die){
+                points += die.value
+              })
+              this.score.threeOfAKind = points
+            }
+          }.bind(this))
+
+          break;
+
+        case 'fourOfAKind':
+
+          this.score.fourOfAKind = 'Bust'
+
+          totals.forEach(function(number){
+            if (number >= 4 ) {
+              diceArray.forEach(function(die){
+                points += die.value
+              })
+              this.score.fourOfAKind = points
+            }
+          }.bind(this))
+
+          break;
+
+        case 'fullHouse':
+
+          if (totals.indexOf(2) !== -1 && totals.indexOf(3) !== -1) {
+            this.score.fullHouse = 25
+
+          } else {
+            this.score.fullHouse = "Bust"
+          }
+
+
+          break;
+
+        case 'smallStraight':
+
+          $log.log(totals.indexOf(1))
+
+          if (num1s >= 1 && num2s >= 1 && num3s >= 1 && num4s >= 1 ) {
+            this.score.smallStraight = 30
+          } else if (num2s >= 1 && num3s >= 1 && num4s >= 1 && num5s >= 1 ) {
+            this.score.smallStraight = 30
+          } else if (num3s >= 1 && num4s >= 1 && num5s >= 1 && num6s >= 1 ) {
+            this.score.smallStraight = 30
+          } else {
+            this.score.smallStraight = "Bust"
+          }
+
+          break;
+
+        case 'largeStraight':
+
+          if (num1s >= 1 && num2s >= 1 && num3s >= 1 && num4s >= 1 && num5s >= 1 ) {
+            this.score.largeStraight = 40
+          } else if (num2s >= 1 && num3s >= 1 && num4s >= 1 && num5s >= 1 && num6s >= 1 ) {
+            this.score.largeStraight = 40
+          } else {
+            this.score.largeStraight = "Bust"
+          }
+
+          break;
+
+        case 'yahtzee':
+
+
+          if (num5s >= 1) {
+            this.score.yahtzee = 50
+          } else {
+            this.score.yahtzee = "Bust"
+          }
+
+          break;
+
+        case 'chance':
+
+          diceArray.forEach(function(die){
+              points += die.value;
+          })
+
+          this.score.chance = points
+
+          break;
 
         default:
           //Statements executed when none of the values match the value of the expression
